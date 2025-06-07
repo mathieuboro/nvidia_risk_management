7b5bc56c885f2ffdb50be72725acfc6ee00b07e9
Marek Dworaczyk <275996@student.pwr.edu.pl>
2025-06-07 11:01:31 +0200

    data import execution

--- data_import.py
+++ data_import.py
@@ -1,17 +1,17 @@
 import yfinance as yf
 
+def get_data():
 
+    samsung_krw = yf.download('005930.KS', period='6y')['Close'].squeeze()
+    usd_krw = yf.download('KRW=X', period='6y')['Close'].squeeze()
+    tsmc_data = yf.download('TSM', period='6y')['Close'].squeeze()
 
-samsung_krw = yf.download('005930.KS', period='6y')['Close'].squeeze()
-usd_krw = yf.download('KRW=X', period='6y')['Close'].squeeze()
-tsmc_data = yf.download('TSM', period='6y')['Close'].squeeze()
+    common_index = samsung_krw.index.intersection(usd_krw.index)
+    samsung_krw_filtered = samsung_krw[common_index]
+    usd_krw_filtered = usd_krw[common_index]
+    samsung_usd = samsung_krw_filtered / usd_krw_filtered
+    samsung_usd_clean = samsung_usd.dropna()
+    samsung = samsung_usd_clean
 
-common_index = samsung_krw.index.intersection(usd_krw.index)
-samsung_krw_filtered = samsung_krw[common_index]
-usd_krw_filtered = usd_krw[common_index]
-samsung_usd = samsung_krw_filtered / usd_krw_filtered
-samsung_usd_clean = samsung_usd.dropna()
-samsung = samsung_usd_clean
-
-tsmc_data.to_csv("tsmc_data.csv")
-samsung.to_csv("samsung_data.csv")
+    tsmc_data.to_csv("tsmc_data.csv")
+    samsung.to_csv("samsung_data.csv")
