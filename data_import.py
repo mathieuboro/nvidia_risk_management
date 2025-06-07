fc65ffcf8fff67a012eeb09439afeae48bcfd191
Marek Dworaczyk <275996@student.pwr.edu.pl>
2025-06-07 10:57:55 +0200

    data import module

---
+++ data_import.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+
+
+
+samsung_krw = yf.download('005930.KS', period='6y')['Close'].squeeze()
+usd_krw = yf.download('KRW=X', period='6y')['Close'].squeeze()
+tsmc_data = yf.download('TSM', period='6y')['Close'].squeeze()
+
+common_index = samsung_krw.index.intersection(usd_krw.index)
+samsung_krw_filtered = samsung_krw[common_index]
+usd_krw_filtered = usd_krw[common_index]
+samsung_usd = samsung_krw_filtered / usd_krw_filtered
+samsung_usd_clean = samsung_usd.dropna()
+samsung = samsung_usd_clean
+
+tsmc_data.to_csv("tsmc_data.csv")
+samsung.to_csv("samsung_data.csv")
